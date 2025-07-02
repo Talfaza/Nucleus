5659a05207303f8b0c4f617c0900ff22879d832b
talfaza <charifyahia4@gmail.com>
2025-07-02 11:52:08 +0100

    new: main file and packages

---
+++ authentification/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/Talfaza/authentification/database"
+	"github.com/Talfaza/authentification/routes"
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	app := fiber.New()
+	database.Connect()
+	routes.Setup(app)
+
+	log.Fatal(app.Listen(":3000"))
+}
