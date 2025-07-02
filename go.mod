ec3f561d6257911238d2b0f61b1f22a180be5677
talfaza <charifyahia4@gmail.com>
2025-07-02 10:25:12 +0100

    new: new test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Talfaza/ci_test
+
+go 1.24.4
---
+++ main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+    result := Add(2, 3)
+    expected := 5
+
+    if result != expected {
+        t.Errorf("Expected %d, got %d", expected, result)
+    }
+}
+
