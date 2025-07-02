34b23f570cb0e8b962f6cf5e7d723fe8f1bbed7b
talfaza <charifyahia4@gmail.com>
2025-07-02 11:53:26 +0100

    new: API rroutes

---
+++ authentification/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"github.com/Talfaza/authentification/controller"
+	"github.com/gofiber/fiber/v3"
+)
+
+func Setup(app *fiber.App) {
+	auth := app.Group("/auth")
+
+	auth.Post("/register", controller.Register)
+	auth.Post("/login", controller.Login)
+	auth.Get("/logout", controller.Logout)
+	auth.Get("/mailcheck", controller.MailCheck)
+}
